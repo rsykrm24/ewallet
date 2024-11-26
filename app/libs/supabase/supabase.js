67b1e655890d5be3fe1884fe5e0af43190845fef
rsykrm24 <128889699+rsykrm24@users.noreply.github.com>
2024-11-26 22:18:38 +0700

    Update supabase.js

--- app/libs/supabase/supabase.js
+++ app/libs/supabase/supabase.js
@@ -1,8 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
-
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
 export default supabase
