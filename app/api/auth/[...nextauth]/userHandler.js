2c3b9fd3b51c2c43f0267d9952790b5aec0b0795
riansyah24 <jackrian72@gmail.com>
2024-11-26 19:12:38 +0700

    update ewallet

--- app/api/auth/[...nextauth]/userHandler.js
+++ app/api/auth/[...nextauth]/userHandler.js
@@ -1,11 +1,27 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/credentials"
 
 const userHandler = NextAuth({
   providers:[
     GoogleProvider({
       clientId:process.env.GOOGLE_CLIENTID,
       clientSecret:process.env.GOOGLE_CLIENTSECRET,
+    }),
+    CredentialsProvider({
+      name:"Credentials",
+      credentials:{
+        email:{label:"Email",type:"email",placeholder:"Email"},
+        password:{label:"Password",type:"password",placeholder:"Password"}
+      },
+      async authorize(credentials, req) {
+        const user = { id: "1", name: "Admin", email: "admin123@admin.com" }
+        if (user) {
+          return user
+        } else {
+          return null
+        }
+      }
     })
     ],
   secret:process.env.NEXTAUTH_SECRET
